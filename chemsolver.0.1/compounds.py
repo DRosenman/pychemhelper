16d2a8b4b977184df9d7f37eddab395a3f259519
Dave Rosenman <rosenmd1@tcnj.edu>
2017-07-29 15:41:23 -0400

    Update compounds.py

--- chemsolver.0.1/compounds.py
+++ chemsolver.0.1/compounds.py
@@ -1,106 +1,109 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 27 17:34:21 2017
+import pandas as pd
+import re
+from chemsolver import elements
 
-@author: rosen
-"""
-
-
-from chemsolver import compounds
-
-def grams_to_moles(mass, chemical):
+def mass(chemical):
     '''
-    :param mass: mass of sample of substance (in grams)
-    :param chemical:  chemical formula of substance
-    :returns: moles of the sample of the substance
+
+    :param chemical: compound chemical symbol (example: 'H2O')
+    :return: molar mass of compound
     '''
-    molar_mass = compounds.mass(chemical)
-    moles = round(mass / molar_mass, 2)
-    return moles
+    chemical = re.findall(r'([A-Z][a-z]*)(\d*)', chemical)
+    m = 0
+    for tup in chemical:
+        if tup[1] == '': 
+            m+=elements.elements[tup[0]].mass
+        else:
+            m+= elements.elements[tup[0]].mass * int(tup[1])
+    return round(m,2)
 
-
-def grams_to_moles_steps(mass, chemical):
-    '''
-    :param mass: mass of sample of substance (in grams)
-    :param chemical:  chemical formula of substance
-    :prints: solution of conversion from grams to moles of the sample of the substance
-    '''
-    molar_mass = compounds.mass(chemical)
-    moles = grams_to_moles(mass, chemical)
-    print(str(mass) + 'g ' + chemical + ' x ' + ' 1 mole ' + chemical + '/' + str(
-        molar_mass) + 'g ' + chemical + ' = ' + str(moles) + ' moles ' + chemical)
-
-
-def moles_to_grams(moles, chemical):
-    return moles * compounds.mass(chemical)
-
-
-def moles_to_grams_steps(moles, chemical):
-    grams = moles_to_grams(moles, chemical)
-    compounds.mass_steps(chemical)
-    mass = compounds.mass(chemical)
-    print("{:.2f} moles x {:.2f} grams/mole = {:.2f} grams".format(moles, mass, grams))
-
-
-def molarity(volume, mass=None, chemical=None, moles=None):
-    '''
-    :param volume: volume of solution (in L)
-    :param mass: mass of the solute (in g), default = None
-    :param chemical: the solute, default = None
-    :param moles: # of moles of solute, default= None
-    :return: molarity of solution (moles of substance/L of solution)
-    '''
-    if mass is None:
-        return moles / volume
-    else:
-        moles = grams_to_moles(mass, chemical)
-        return moles / volume
-
-
-def molarity_steps(volume, mass=None, chemical=None, moles=None):
-    '''
-    :param volume: volume of solution (in L)
-    :param mass: mass of the solute (in g), default = None
-    :param chemical: the solute, default = None
-    :param moles: # of moles of solute, default= None
-    :prints: step by step determination of the molarity of the solution
-    '''
-    if moles is None:
-        grams_to_moles_steps(mass, chemical)
-        moles = grams_to_moles(mass, chemical)
-        M = molarity(volume, mass, chemical)
-    else:
-        M = molarity(volume, moles=moles)
-    print("{:.2f} moles/{:.2f} L = {:.2f} M".format(moles, volume, M))
-
-
-def molarity_to_moles(M, volume):
+def mass_steps(chemical):
     """
-    :param M: the concentration of the solution in moles per L
-    :param volume: the volume of the solution in Liters
-    returns: moles of solute
+
+    :param chemical: compound chemical symbol (example: 'H2O')
+    :prints: Step by step calculation of compound's mass
     """
-    return M * volume
+    chem = re.findall(r'([A-Z][a-z]*)(\d*)', chemical)
+    m = 0
+    numbers = []
+    print(chemical)
+    for tup in chem:
+        if tup[1] == '':
+            print(tup[0] + ': ' + str(elements.elements[tup[0]].mass))
+            m+=elements. elements[tup[0]].mass
+            numbers.append(elements.elements[tup[0]].mass)
+
+        else:
+            print(tup[0] + ': ' + tup[1] + 'x' + str(elements.elements[tup[0]].mass) + ' = ' + str(elements.
+                elements[tup[0]].mass * int(tup[1])))
+            m+= elements.elements[tup[0]].mass * int(tup[1])
+            numbers.append(elements.elements[tup[0]].mass * int(tup[1]))
+    n = len(numbers)
+    i = 1
+
+    string = str(numbers[0])
+    while i < n:
+        string = string + ' + ' +  str(numbers[i ])
+        i+=1 
+    print( '\nMass ' + chemical + ' = ' + string + ' = ' + str(round(m,2)))
 
 
-def molarity_to_moles_steps(M, volume):
-    print("{:.2f} moles/L x {:.2f} L = {:.2f} moles".format(M, volume, molarity_to_moles(M, volume)))
-
-
-def molarity_to_grams(molarity, volume, chemical):
+def composition(chemical):
     '''
-    :param molarity: molarity of solution (moles of solute/ L of solution)
-    :param volume: volume of solution (in L)
-    :param chemical: chemical formula of solute
-    :return: mass of solute (in grams)
+    :param chemical: chemical compound symbol (example 'H2O')
+    :return: pandas Series containing mass due to each element in compound and total mass of compound
     '''
-    molar_mass = compounds.mass(chemical)
-    moles = molarity * volume
-    return round(molar_mass * moles, 2)
+    total_mass = mass(chemical)
+    chem = re.findall(r'([A-Z][a-z]*)(\d*)', chemical)
+    masses = []
+    ind = []
+    for tup in chem:
+        if tup[1] == '':
+            masses.append(elements.elements[tup[0]].mass)
+            ind.append(tup[0])
+
+        else:
+            masses.append(elements.elements[tup[0]].mass * int(tup[1]))
+            ind.append(tup[0])
+    ind.append('Total:')
+    masses.append(total_mass)
+    return pd.Series(masses,index=ind,name= chemical) 
 
 
-def molarity_to_grams_steps(molarity, volume, chemical):
-    moles = molarity_to_moles(molarity, volume)
-    moles_to_grams_steps(moles, chemical)
+def composition_steps(chemical):
+    '''
+    :param chemical: chemical compound symbol (example 'H2O')
+    :prints: step by step solution of mass due to each element in compound and total mass
+    '''
+    mass_steps(chemical)
+    print('')
+    print(composition(chemical))
+
+
+def percent_composition(chemical):
+    '''
+    :param chemical: chemical compound symbol (example 'H2O')
+    :return: pandas Series containing percent composition of each element making up compound.
+    '''
+    return round((composition(chemical)/mass(chemical))* 100,3)
+
+
+def percent_composition_steps(chemical):
+    """
+    :param chemical: chemical compound symbol (example 'H2O')
+    :prints: step by step solution of solving the percent composition of each element making up the compound.
+    """
+    composition_steps(chemical)
+    comp = composition(chemical)
+    component_series = comp.drop('Total:')
+    i=0
+    for component in component_series:
+        el = component_series.index[i]
+        el_mass = component_series[i]
+        chem_mass = mass(chemical)
+
+        comp_str = "{}: 100 x {:.2f}g {}/{:.2f}g {} = {:.2f}%".format(el,el_mass,el,chem_mass,chemical,percent_composition(chemical)[i])
+        print(comp_str)
+        i +=1
 
 
